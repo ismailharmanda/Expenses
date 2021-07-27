35dc5322156dfcd55056e807386776b4c8b21614
ismail harmanda <iharmanda@hotmail.com>
2021-07-27 21:49:34 +0300

    no more hard coded texts

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,17 @@
 import "./ExpenseItem.css";
 const ExpenseItem = () => {
+  const expenseDate = new Date(2021, 7, 27);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <div>{expenseDate.toDateString()}</div>
       <div
         className="expense-item__descr
       ption"
       >
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
