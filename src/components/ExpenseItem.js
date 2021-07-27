0c80ed9947907c194945039d90e6cf6432eef072
ismail harmanda <iharmanda@hotmail.com>
2021-07-27 21:31:55 +0300

    change content

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,10 @@
 const ExpenseItem = () => {
   return (
     <div>
-      <div>Date</div>
+      <div>March 28th 2021</div>
       <div>
-        <h2>Title</h2>
-        <div>Amount</div>
+        <h2>Car Insurance</h2>
+        <div>$294.67</div>
       </div>
     </div>
   );
