a11f3d4ab5151d5d8b89da73a717b108512c3c75
ismail harmanda <iharmanda@hotmail.com>
2021-07-27 22:32:48 +0300

    create and implement ExpenseDate.css

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,12 +1,13 @@
+import "./ExpenseDate.css";
 const ExpenseDate = (props) => {
   const month = props.date.toLocaleString("tr-TR", { month: "long" });
   const year = props.date.toLocaleString("tr-TR", { year: "numeric" });
   const day = props.date.toLocaleString("tr-TR", { day: "numeric" });
   return (
-    <div>
-      <div>{month}</div>
-      <div>{year}</div>
-      <div>{day}</div>
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
     </div>
   );
 };
