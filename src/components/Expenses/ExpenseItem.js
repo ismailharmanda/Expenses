ded9513a72f3e5fb4e234f717ef0b0d3fc908add
ismail harmanda <iharmanda@hotmail.com>
2021-07-29 12:28:22 +0300

    typo in className, onClick test

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,16 +2,17 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log("clicked");
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} title={props.title} day={props.day} />
-      <div
-        className="expense-item__descr
-      ption"
-      >
+      <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
