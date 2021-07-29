ef23faf3162f4bc01dfc1d780c50bb4f04dd7e89
ismail harmanda <iharmanda@hotmail.com>
2021-07-29 23:56:50 +0300

    render list of data

--- src/components/Expenses/DisplayExpenses.js
+++ src/components/Expenses/DisplayExpenses.js
@@ -12,16 +12,13 @@ const DisplayExpenses = (props) => {
     <div>
       <Card className="display-expenses">
         <ExpenseFilter selected={filteredYear} onChange={onChangeHandler} />
-        <ExpenseItem
-          date={props.expenses[0].date}
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-        />
-        <ExpenseItem
-          date={props.expenses[1].date}
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-        />
+        {props.expenses.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            date={expense.date}
+            amount={expense.amount}
+          />
+        ))}
       </Card>
     </div>
   );
