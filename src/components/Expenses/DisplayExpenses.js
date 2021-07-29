55d4940143bb3c43a3ef9c8018a59a351bf839c2
ismail harmanda <iharmanda@hotmail.com>
2021-07-29 19:13:47 +0300

    Date select and filter implemented

--- src/components/Expenses/DisplayExpenses.js
+++ src/components/Expenses/DisplayExpenses.js
@@ -1,20 +1,29 @@
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./DisplayExpenses.css";
+import ExpenseFilter from "./ExpenseFilter";
 const DisplayExpenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2021");
+  const onChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
   return (
-    <Card className="display-expenses">
-      <ExpenseItem
-        date={props.expenses[0].date}
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-      />
-      <ExpenseItem
-        date={props.expenses[1].date}
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-      />
-    </Card>
+    <div>
+      <Card className="display-expenses">
+        <ExpenseFilter selected={filteredYear} onChange={onChangeHandler} />
+        <ExpenseItem
+          date={props.expenses[0].date}
+          title={props.expenses[0].title}
+          amount={props.expenses[0].amount}
+        />
+        <ExpenseItem
+          date={props.expenses[1].date}
+          title={props.expenses[1].title}
+          amount={props.expenses[1].amount}
+        />
+      </Card>
+    </div>
   );
 };
 export default DisplayExpenses;
