173b7648b96f6ba0711a1010aa4d37095f3e19c8
ismail harmanda <iharmanda@hotmail.com>
2021-07-29 15:40:12 +0300

    Child-to-Parent Component Communication (Bottom-up)

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   //   const [enteredTitle, setEnteredTitle] = useState("");
   //   const [enteredAmount, setEnteredAmount] = useState("");
   //   const [enteredDate, setEnteredDate] = useState(new Date());
@@ -32,7 +32,7 @@ const ExpenseForm = () => {
     const expenseData = {
       ...userInput,
     };
-    console.log(expenseData);
+    props.onSaveExpenseData(expenseData);
     setUserInput({
       enteredTitle: "",
       enteredAmount: "",
