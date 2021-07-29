645fa262b6aea097a970cd94d448b1d206f8b238
ismail harmanda <iharmanda@hotmail.com>
2021-07-29 14:53:15 +0300

    Use one state insted of 3 sperated states

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,22 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  //   const [enteredTitle, setEnteredTitle] = useState("");
+  //   const [enteredAmount, setEnteredAmount] = useState("");
+  //   const [enteredDate, setEnteredDate] = useState(new Date());
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
+  });
+  const { enteredTitle, enteredAmount, enteredDate } = userInput;
+
+  const onTitleChange = (e) => {
+    setUserInput({ ...userInput, enteredTitle: e.target.value });
+  };
+  const onAmountChange = (e) => {
+    setUserInput({ ...userInput, enteredAmount: e.target.value });
+  };
+  const onDateChange = (e) => {
+    setUserInput({ ...userInput, enteredDate: e.target.value });
+  };
   return (
     <div>
       <form>
         <div className="new-expense__controls">
           <div className="new-expense__control">
             <label>Title</label>
-            <input type="text"></input>
+            <input
+              value={enteredTitle}
+              type="text"
+              onChange={(e) => onTitleChange(e)}
+            ></input>
           </div>
           <div className="new-expense__control">
             <label>Amount</label>
-            <input type="number" min="0.01" step="0.01"></input>
+            <input
+              value={enteredAmount}
+              type="number"
+              min="0.01"
+              step="0.01"
+              onChange={(e) => onAmountChange(e)}
+            ></input>
           </div>
           <div className="new-expense__control">
             <label>Date</label>
-            <input type="date" min="2019-01-01" max="2022-12-31"></input>
+            <input
+              value={enteredDate}
+              type="date"
+              min="2019-01-01"
+              max="2022-12-31"
+              onChange={(e) => onDateChange(e)}
+            ></input>
           </div>
         </div>
         <div className="new-expense__actions">
