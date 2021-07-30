b5b64fd400314c2baa03d65827007b4a74986308
ismail harmanda <iharmanda@hotmail.com>
2021-07-30 13:48:24 +0300

    deleted useless button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,11 +3,6 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  const clickHandler = () => {
-    setTitle("Updated");
-    console.log(title);
-  };
   return (
     <li>
       <Card className="expense-item">
@@ -16,7 +11,6 @@ const ExpenseItem = (props) => {
           <h2>{props.title}</h2>
           <div className="expense-item__price">${props.amount}</div>
         </div>
-        <button onClick={clickHandler}>Change Title</button>
       </Card>
     </li>
   );
